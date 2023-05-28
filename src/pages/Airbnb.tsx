6f884451770332f63d96286984e813e07211a489
GUANTAI001 <jpguantai@gmail.com>
2023-05-29 00:27:48 +0300

    inial

--- src/pages/Airbnb.tsx
+++ src/pages/Airbnb.tsx
@@ -1,13 +1,24 @@
 import React, { useState } from 'react';
+import { Table, Input, } from "antd";
+
 
 
 
 const Airbnb = () => {
 
     const [page, setPage] = useState(1);
+    const [activeButton, setActiveButton] = useState(1);
+
+    const handleClick1 = () => {
+        setPage(1);
+        // change color of button when clicked
+        setActiveButton(1);
+    };
 
     const handleClick = () => {
-        setPage(page === 1 ? 2 : 1);
+        setPage(2);
+        // change color of button when clicked
+        setActiveButton(2);
     };
 
 
@@ -15,49 +26,102 @@ const Airbnb = () => {
     return (
         <div className="justify-evenly w-full p-2 ">
             <p className="text-xl m-4 ">Airbnb</p>
-
             <div className=" bg-white rounded-lg shadow-sm">
-                <p className=" text-xl p-2 text-white w-full bg-[#95873C]">Airbnb Listing </p>
-                <div className="flex justify-center mt-4">
-                    <div className="bg-white rounded-lg shadow-sm">
-                        <div className="flex justify-center mt-4">
-                            {page === 1 && (
-                                <div>
-                                    <h1>Page 2</h1>
-                                    <h1 className="text-xl m-4 ">Air   bnb</h1>
-                                    <h1 className="text-xl m-4 ">Air   bnb</h1>
-                                    <h1 className="text-xl m-4 ">Airbnb</h1>
-                                    <h1 className="text-xl m-4 ">Ai   rbnb</h1>
-                                    <h1 className="text-xl m-4 ">Airb   nb</h1>
-                                    <h1 className="text-xl m-4 ">Air    bnb</h1>
-                                    <h1 className="text-xl m-4 ">Airbnb</h1>
-                                    <h1 className="text-xl m-4 ">Ai  r bnb</h1>
-                                    <h1 className="text-xl m-4 ">Ai  rb   nb</h1>
-                                    <h1 className="text-xl m-4 ">Air     bnb</h1>
-                                    <h1 className="text-xl m-4 ">Air    bnb</h1>
-                                    <h1 className="text-xl m-4 ">Airb   nb</h1>
+                <p className=" text-xl p-2 text-white w-full bg-[#95873C]">AirBnbs Listing </p>
+                <div className="flex flex-row mt-4 ml-8">
+                    <button
+                        className={` p-2  ${activeButton === 1 ? 'bg-[#95873C] text-white' : 'bg-white text-black'
+                            }`}
+                        onClick={handleClick1}
+                    >
+                        Hotel List
+                    </button>
+                    <button
+                        className={` p-2  ${activeButton === 2 ? 'bg-[#95873C] text-white' : 'bg-white text-black'
+                            }`}
+                        onClick={handleClick}
+                    >
+                        Add Hotel
+                    </button>
+                </div>
+
+                <div className="flex flex-col mx-8 justify-center">
+                    <div className="flex flex-col justify-center">
+                        {page === 1 && (
+                            <Table
+                                columns={[
+                                    {
+                                        title: "Hotel Name",
+                                        dataIndex: "name",
+                                    },
+                                    {
+                                        title: "Location",
+                                        dataIndex: "location",
+                                    },
+                                    {
+                                        title: "Price",
+                                        dataIndex: "price",
+                                    },
+                                    {
+                                        title: "Beds",
+                                        dataIndex: "beds",
+                                    },
+                                    {
+                                        title: "Status",
+                                        dataIndex: "status",
+                                    },
+                                    {
+                                        title: "Action",
+                                        dataIndex: "action",
+                                    },
+
+
+                                ]}
+                            >   </Table>
+                        )}
+
+                        {page === 2 && (
+                            <div>
+                                <div className="flex flex-row m-4">
+                                    <div className="flex flex-col flex-grow">
+                                        <label className="text-sm">Hotel Name</label>
+                                        <Input className="" placeholder="Hotel Name" />
+                                    </div>
+                                    <div className="flex flex-col ml-4 flex-grow">
+                                        <label className="text-sm">Location</label>
+                                        <Input className="" placeholder="Location" />
+                                    </div>
+                                    <div className="flex flex-col ml-4 flex-grow">
+                                        <label className="text-sm">Price</label>
+                                        <Input className="" placeholder="Price" />
+                                    </div>
                                 </div>
-                            )}
-                            {page === 2 && (
-                                <div>
-                                    <h1>Page 2</h1>
-                                    {/* Content for Page 2 */}
-                                    <h1 className="text-xl m-4 ">Airbnb</h1>
-                                    <h1 className="text-xl m-4 ">Airbnb</h1>
-                                    <h1 className="text-xl m-4 ">Airbnb</h1>
-                                    <h1 className="text-xl m-4 ">Airbnb</h1>
-                                    <h1 className="text-xl m-4 ">Airbnb</h1>
-                                    <h1 className="text-xl m-4 ">Airbnb</h1>
-                                    <h1 className="text-xl m-4 ">Airbnb</h1>
-                                    <h1 className="text-xl m-4 ">Airbnb</h1>
-                                    <h1 className="text-xl m-4 ">Airbnb</h1>
-                                    <h1 className="text-xl m-4 ">Airbnb</h1>
-                                    <h1 className="text-xl m-4 ">Airbnb</h1>
-                                    <h1 className="text-xl m-4 ">Airbnb</h1>
+                                <div className="flex flex-row m-4">
+                                    <div className="flex flex-col flex-grow">
+                                        <label className="text-sm">Beds</label>
+                                        <Input className="" placeholder="Beds" />
+                                    </div>
+                                    <div className="flex flex-col ml-4 flex-grow">
+                                        <label className="text-sm"> About </label>
+                                        <Input className="" placeholder="About" />
+                                    </div>
                                 </div>
-                            )}
-                        </div>
-                        <button onClick={handleClick}>Change Page</button>
+
+
+                                <div className="flex flex-flex-col ml-4 ">
+
+                                    <div className="flex flex-col flex-grow">
+                                        <label className="text-sm">Upload Image</label>
+                                        <Input className="file" placeholder="Upload Image" />
+                                    </div>
+                                </div>
+
+
+
+
+
+                            </div>
+                        )}
                     </div>
                 </div>
             </div>
